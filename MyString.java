3dda26c682815027f6c52ecba0c23d3b5a2636ad
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2024-12-05 19:33:51 +0200

    Done

--- MyString.java
+++ MyString.java
@@ -19,12 +19,40 @@ public class MyString {
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
         // Replace the following statement with your code
-        return null;
+        String ans ="";
+		char ch = str.charAt(0);
+		if (ch >= 'a' && ch <='z'|| ch >='!' && ch <= '@')ans = "" + ch;
+		else if(ch >= 'A' && ch <= 'Z') 
+		{
+			ch+=32;
+			ans = "" + ch;
+		}
+		int i=1;
+		while (i < str.length()) {
+			ch= str.charAt(i);
+			if (ch >= 'A' && ch <= 'Z'){
+				ch+=32;
+				ans = ans + ch;
+			}
+			else if (ch >= 'a' && ch <='z' || ch >='!' && ch <= '@') ans = ans + ch;
+			i++;
+		}
+		return ans;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+        if(str1.length()<str2.length()) return false;
+        else if(str1.equals(str2)) return true;
+        else{
+            String str="";
+            int options= str1.length()-str2.length()+1;
+            for(int i=0; i< options; i++){
+                str = str1.substring(i, str2.length()+i);
+                if(str.equals(str2))return true;
+            }
+        }
         return false;
     }
 }
